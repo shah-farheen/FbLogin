ab093354eda2313199f4f27337476aab6d0f0cf7
farheen-shah <farheenshahdelhi@gmail.com>
2016-06-13 19:48:38 +0530

    random

--- app/src/main/java/com/bits/farheen/fblogin/MainActivity.java
+++ app/src/main/java/com/bits/farheen/fblogin/MainActivity.java
@@ -1,16 +1,29 @@
 package com.bits.farheen.fblogin;
 
+import android.content.Intent;
+import android.content.pm.PackageInstaller;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.LoggingBehavior;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     LoginButton loginButton;
     TextView textName;
     String TAG = "MainActivity";
+    ProfileTracker profileTracker;
+    AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +43,30 @@ public class MainActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         textName = (TextView) findViewById(R.id.text_name);
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            }
+        };
+        accessTokenTracker.startTracking();
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+
+            }
+        };
+        profileTracker.startTracking();
+
+        FacebookSdk.addLoggingBehavior(LoggingBehavior.REQUESTS);
+        loginButton.setReadPermissions("user_friends","user_education_history","user_likes");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.wtf(TAG,"onSuccess");
+                Profile profile = Profile.getCurrentProfile();
+                makeRequest(AccessToken.getCurrentAccessToken(), profile.getId());
             }
 
             @Override
@@ -45,5 +80,29 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"onError");
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    public void makeRequest(AccessToken accessToken, String userId){
+        GraphRequest request = new GraphRequest(accessToken, "/"+userId+"/likes", null, HttpMethod.GET
+                , new GraphRequest.Callback() {
+            @Override
+            public void onCompleted(GraphResponse response) {
+                Log.d(TAG, response.toString());
+            }
+        });
+        request.executeAsync();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
     }
 }
